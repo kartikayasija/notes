6d1ff4e39555a228455e802e57eb5cf97d3461eb
Kartikay Asija <kartikay7057@gmail.com>
2023-04-21 22:06:03 +0530

    home route

--- Server/app.js
+++ Server/app.js
@@ -13,7 +13,9 @@ mongoose.connect(process.env.MONGODB).catch((err)=>{
 })
 app.use(cors());
 app.use("/notes",notes.router);
-
-app.listen(process.env.PORT,()=>{
+app.get("/",(req,res)=>{
+  res.send("hello");
+})
+app.listen(process.env.PORT || 3000,()=>{
   console.log("started");
 })
